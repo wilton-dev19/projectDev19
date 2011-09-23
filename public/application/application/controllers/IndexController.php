c16303b33da5e3d6b0a0892176ea4cdecdd01305
Wilton Saint-vil <saintvil.wilton@gmail.com>
2011-09-23 14:54:57 +0200

    Ajout de application.ini

---
+++ public/application/application/controllers/IndexController.php
@@ -0,0 +1,10 @@
+<?php
+
+// la classe porte le nom du fichier
+class IndexController extends Zend_Controller_Action
+{
+	// fonction public correspondant à l'action qui va déclencher une vue
+	public function indexAction()
+	{
+	}
+}
